fed3ae0c98c09c7f1c702847fd8803786c0fd0c3
Yesenia <61083228+YeseniaCM@users.noreply.github.com>
2023-11-06 15:14:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function($){
+    $(".meny-knapp").click( function() {
+        $(".site-nav").toggle("show");
+    });
+}); 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("bildbox");
+  var dots = document.getElementsByClassName("punk");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
